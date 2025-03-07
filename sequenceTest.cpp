ce075b463fc743e11b9a9596d87d7ab9675a3b52
Syd-108 <xtg15@txstate.edu>
2025-03-06 18:09:44 -0600

    Update sequenceTest.cpp

--- sequenceTest.cpp
+++ sequenceTest.cpp
@@ -41,59 +41,182 @@ int main()
         choice = toupper(get_user_command());
 
         switch(choice)
+{
+    case '!':
+        objectNum = get_object_num();
+        if (objectNum == 1)
         {
-            case '!':
-                objectNum = get_object_num();
-                (objectNum == 1) ? s1.start() : s2.start();
-                cout << "Sequence " << objectNum << " started." << endl;
-                break;
-            case '&':
-                objectNum = get_object_num();
-                (objectNum == 1) ? s1.end() : s2.end();
-                cout << "Sequence " << objectNum << " ended." << endl;
-                break;
-            case '+':
-                objectNum = get_object_num();
-                if (objectNum == 1 && s1.is_item()) s1.advance();
-                else if (objectNum == 2 && s2.is_item()) s2.advance();
-                else cout << "Cannot advance." << endl;
-                break;
-            case '-':
-                objectNum = get_object_num();
-                if (objectNum == 1 && s1.is_item()) s1.move_back();
-                else if (objectNum == 2 && s2.is_item()) s2.move_back();
-                else cout << "Cannot move back." << endl;
-                break;
-            case '?':
-                objectNum = get_object_num();
-                cout << ((objectNum == 1 ? s1.is_item() : s2.is_item())
-                        ? "Has item." : "No current item.") << endl;
-                break;
-            case 'C':
-                objectNum = get_object_num();
-                if (objectNum == 1 && s1.is_item()) cout << "Current item: " << s1.current() << endl;
-                else if (objectNum == 2 && s2.is_item()) cout << "Current item: " << s2.current() << endl;
-                else cout << "No current item." << endl;
-                break;
-            case 'P':
-                objectNum = get_object_num();
-                (objectNum == 1) ? show_list(s1) : show_list(s2);
-                break;
-            case 'A':
-                objectNum = get_object_num();
-                if(objectNum == 1) s1.add(get_number());
-                else s2.add(get_character());
-                break;
-            case 'R':
-                objectNum = get_object_num();
-                if (objectNum == 1 && s1.is_item()) s1.remove_current();
-                else if (objectNum == 2 && s2.is_item()) s2.remove_current();
-                else cout << "No current item to remove." << endl;
-                break;
-            case 'Q':
-                cout << "Exiting..." << endl; break;
-            default:
-                cout << "Invalid choice." << endl;
+            s1.start();
+            cout << "s1 started." << endl;
+        }
+        else
+        {
+            s2.start();
+            cout << "s2 started." << endl;
+        }
+        break;
+
+    case '&':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            s1.end();
+            cout << "s1 ended." << endl;
+        }
+        else
+        {
+            s2.end();
+            cout << "s2 ended." << endl;
+        }
+        break;
+
+    case '+':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            if (s1.is_item())
+            {
+                s1.advance();
+                cout << "s1 advanced one item." << endl;
+            }
+            else
+                cout << "Can't advance s1." << endl;
+        }
+        else
+        {
+            if (s2.is_item())
+            {
+                s2.advance();
+                cout << "s2 advanced one item." << endl;
+            }
+            else
+                cout << "Can't advance s2." << endl;
+        }
+        break;
+
+    case '-':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            if (s1.is_item())
+            {
+                s1.move_back();
+                cout << "s1 moved back one item." << endl;
+            }
+            else
+                cout << "Can't move back s1." << endl;
+        }
+        else
+        {
+            if (s2.is_item())
+            {
+                s2.move_back();
+                cout << "s2 moved back one item." << endl;
+            }
+            else
+                cout << "Can't move back s2." << endl;
+        }
+        break;
+
+    case '?':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            if (s1.is_item())
+                cout << "s1 has a current item." << endl;
+            else
+                cout << "s1 has no current item." << endl;
+        }
+        else
+        {
+            if (s2.is_item())
+                cout << "s2 has a current item." << endl;
+            else
+                cout << "s2 has no current item." << endl;
+        }
+        break;
+
+    case 'C':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            if (s1.is_item())
+                cout << "Current item in s1 is: " << s1.current() << endl;
+            else
+                cout << "s1 has no current item." << endl;
+        }
+        else
+        {
+            if (s2.is_item())
+                cout << "Current item in s2 is: " << s2.current() << endl;
+            else
+                cout << "s2 has no current item." << endl;
+        }
+        break;
+
+    case 'P':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            cout << "s1 contains: ";
+            show_list(s1);
+        }
+        else
+        {
+            cout << "s2 contains: ";
+            show_list(s2);
+        }
+        break;
+
+    case 'A':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            numHold = get_number();
+            s1.add(numHold);
+            cout << numHold << " added to s1." << endl;
+        }
+        else
+        {
+            charHold = get_character();
+            s2.add(charHold);
+            cout << charHold << " added to s2." << endl;
+        }
+        break;
+
+    case 'R':
+        objectNum = get_object_num();
+        if (objectNum == 1)
+        {
+            if (s1.is_item())
+            {
+                numHold = s1.current();
+                s1.remove_current();
+                cout << numHold << " removed from s1." << endl;
+            }
+            else
+                cout << "s1 has no current item." << endl;
+        }
+        else
+        {
+            if (s2.is_item())
+            {
+                charHold = s2.current();
+                s2.remove_current();
+                cout << charHold << " removed from s2." << endl;
+            }
+            else
+                cout << "s2 has no current item." << endl;
+        }
+        break;
+
+    case 'Q':
+        cout << "Quit option selected...bye" << endl;
+        break;
+
+    default:
+        cout << choice << " is invalid...try again" << endl;
+
         }
     } while(choice != 'Q');
 }
